f556f9263cacc36cfe09c3508ad5c14a54c48f23
nico_qwer <nicolas.hebert@outlook.com>
2022-04-08 17:51:34 -0400

    File class started

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+using static System.Console;
+
+namespace MiniComputer
+{
+    internal class Program
+    {
+        static string currentDirectory = "Main/";
+        static File? openFile;
+
+        static void Main(string[] args)
+        {
+            Title = "MiniComputer";
+
+            WriteLine("Booting...");
+            WriteLine("Booted!");
+
+            Write(currentDirectory + " : ");
+            ReadLine();
+
+            ReadLine();
+        }
+
+        public static void WriteError(string text)
+        {
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine(text);
+            ResetColor();
+        }
+    }
+
+    class File 
+    {
+        public static List<File> files = new List<File>();
+
+        public string name;
+        public string directory;
+        public int size;
+        public string type;
+
+        public File(string newName, string newDirectory)
+        {
+            Rename(newName);
+            directory = newDirectory;
+
+        }
+
+        public void Rename(string newName) 
+        {
+            if (newName == null || newName == " ")
+            {
+                Program.WriteError("Cannot rename to nothing");
+                return;
+            }
+
+            string newExtension = newName.Split('.').Last();
+
+            if (newExtension != null)
+            {
+                name = newName;
+                type = newExtension;
+            } else
+            {
+                name = newName + "." + type;
+            }
+        }
+    }
+}
